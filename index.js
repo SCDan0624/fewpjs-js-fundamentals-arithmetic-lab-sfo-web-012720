abd2ce1fd0bed2884babfff5181ceb101bc80fb6
SCDan0624 <dmayle1@hotmail.com>
2020-03-19 15:00:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+var multiply = num1 * num2;
+var random = Math.floor(Math.random() * 1000) + 1;
+var mod = 16% 6;
+var max = Math.max(5,10,15,20);
